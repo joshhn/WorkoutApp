04f2d8c2fef149e8fd4a21bc325499d6d0028868
joshhn <duy.huynhngoc.05@gmail.com>
2022-06-28 16:50:42 -0400

    Add mainActivity

--- app/src/main/java/com/joshhn/workoutapp/MainActivity.kt
+++ app/src/main/java/com/joshhn/workoutapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.joshhn.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.joshhn.workoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener{
+            Toast.makeText(this,"Worked", Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
