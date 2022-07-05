b83531879f31921059ab75d69d5d25df59e5f436
joshhn <duy.huynhngoc.05@gmail.com>
2022-07-05 14:07:01 -0400

    Add Back Pressed Confirmation Dialog

--- app/src/main/java/com/joshhn/workoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/joshhn/workoutapp/ExerciseActivity.kt
@@ -1,5 +1,6 @@
 package com.joshhn.workoutapp
 
+import android.app.Dialog
 import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
@@ -12,6 +13,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.joshhn.workoutapp.databinding.ActivityExerciseBinding
+import com.joshhn.workoutapp.databinding.DialogCustomBackConfirmationBinding
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -51,7 +53,7 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
         //Add back Button to Toolbar
         binding?.toolbarExercise?.setNavigationOnClickListener {
-            onBackPressed()
+            customDialogForBackButton()
         }
 
         exerciseList = Constants.defaultExerciseList()
@@ -62,6 +64,25 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         speakOut("Go")
         setupRestView()
         setupExerciseRecyclerView()
+    }
+
+    private fun customDialogForBackButton() {
+        val customDialog = Dialog(this)
+        val dialogBinding = DialogCustomBackConfirmationBinding.inflate(layoutInflater)
+        customDialog.setContentView(dialogBinding.root)
+        customDialog.setCanceledOnTouchOutside(false)
+        dialogBinding.btnYes.setOnClickListener{
+            this@ExerciseActivity.finish()
+            customDialog.dismiss()
+        }
+        dialogBinding.btnNo.setOnClickListener {
+            customDialog.dismiss()
+        }
+    }
+
+    override fun onBackPressed() {
+        customDialogForBackButton()
+        //super.onBackPressed()
     }
 
     private fun setupExerciseRecyclerView(){
